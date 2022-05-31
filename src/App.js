289fa105788ea03fc70c6028b433c2e9e4aa0fc1
Khumendra Sinha <khumendrasinha78@gmail.com>
2022-05-31 16:31:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(prpos) {
+    super(prpos);
+
+    // State
+
+    this.state = {
+      message: "Off",
+      img_url: "https://www.w3schools.com/js/pic_bulboff.gif",
+    };
+  }
+
+  handleGreetingOff = () => {
+    this.state = this.setState({
+      message: "Off",
+      img_url: "https://www.w3schools.com/js/pic_bulboff.gif",
+    });
+  };
+
+  handleGreetingOn = () => {
+    this.state = this.setState({
+      message: "On",
+      img_url: "https://www.w3schools.com/js/pic_bulbon.gif",
+    });
+  };
+
+  render() {
+    return (
+      <div align="center">
+        <h1>Bulb [{this.state.message}] </h1>
+        <img src={this.state.img_url} />
+        <p>{this.state.message}</p>
+        <button
+          onClick={this.handleGreetingOn}
+          style={{
+            border: "white",
+            backgroundColor: "yellow",
+            padding: "5px 30px",
+            marginRight: "5px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          On
+        </button>
+        <button
+          onClick={this.handleGreetingOff}
+          style={{
+            border: "white",
+            backgroundColor: "",
+            padding: "5px 30px",
+          }}
+        >
+          Off
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
